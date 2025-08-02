f8f40240eed24ebcead91201e234b3a24ecd7551
nwnisworking <nwnisworking@gmail.com>
2025-08-02 22:17:56 +0800

    reworking the branch. Check out the new branch!

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Text Editor
+
+An online text editor created using components to replicate the actions of a normal text editor. This project is spurred by one of the member on Discord. 
+
--- js/main.js
+++
@@ -1,7 +0,0 @@
-import EditorCursor from "./Editor/Elements/EditorCursor.js"
-import TextEditor from "./Editor/TextEditor.js"
-import TextRow from "./Editor/Elements/TextRow.js"
-
-customElements.define("editor-cursor", EditorCursor)
-customElements.define("text-editor", TextEditor)
-customElements.define("text-row", TextRow)
